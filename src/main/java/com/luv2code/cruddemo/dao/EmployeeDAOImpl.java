f1c6a6b8e2218daf0f329b38336248bfed810e4e
ltn.lenghia2@gmail.comgit config --global user.email ltn.lenghia2@gmail.com <138308068+ltnghia23@users.noreply.github.com>
2025-05-17 21:04:36 +0700

    swap from manual JPA to Spring Data JPA

---
+++ src/main/java/com/luv2code/cruddemo/dao/EmployeeDAOImpl.java
@@ -0,0 +1,44 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO{
+
+    EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOImpl(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> query = entityManager.createQuery("from Employee", Employee.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        TypedQuery<Employee> query = entityManager.createQuery("from Employee where id = :theId", Employee.class);
+        query.setParameter("theId", id);
+        return query.getSingleResult();
+    }
+
+    @Override
+    public Employee save(Employee employee) {
+        return entityManager.merge(employee);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Employee employee = entityManager.find(Employee.class, id);
+        entityManager.remove(employee);
+    }
+}
