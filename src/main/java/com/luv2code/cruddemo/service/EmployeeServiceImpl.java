cb418213ae3cfadad0feb44684d0c5b067e0682a
ltn.lenghia2@gmail.comgit config --global user.email ltn.lenghia2@gmail.com <138308068+ltnghia23@users.noreply.github.com>
2025-05-17 21:52:50 +0700

    last update

--- src/main/java/com/luv2code/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/cruddemo/service/EmployeeServiceImpl.java
@@ -1,42 +1,47 @@
 package com.luv2code.cruddemo.service;
 
-import com.luv2code.cruddemo.dao.EmployeeDAO;
+import com.luv2code.cruddemo.dao.EmployeeRepository;
 import com.luv2code.cruddemo.entity.Employee;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO){
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int id) {
-        return employeeDAO.findById(id);
+        Optional<Employee> result = employeeRepository.findById(id);
+        Employee theEmployee = null;
+        if(result.isPresent()){
+            theEmployee = result.get();
+        }else {
+            throw new RuntimeException("Did not find employee Id " + id);
+        }
+        return theEmployee;
     }
 
     @Override
-    @Transactional
     public Employee save(Employee employee) {
-        return employeeDAO.save(employee);
+        return employeeRepository.save(employee);
     }
 
     @Override
-    @Transactional
     public void deleteById(int id) {
-        employeeDAO.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
